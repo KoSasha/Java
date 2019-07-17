eb43a34f65474b8c21ff762464a26ed87684ef30
KoSasha <sasha_pishi99@mail.ru>
2019-07-17 16:50:08 +0700

    index.html.

--- WebServer/src/main/java/WebServer.java
+++ WebServer/src/main/java/WebServer.java
@@ -8,6 +8,9 @@ public class WebServer {
 
     private static Socket client;
 
+    private static String request;
+
+    // ввести в браузере: "http://localhost:4040/src/main/resources/index.html"
     public static void main(String[] args) throws IOException {
         server = new ServerSocket(4040);
         System.out.println("Погнали");
@@ -20,22 +23,45 @@ public class WebServer {
             BufferedReader in = new BufferedReader(inStream);
             BufferedWriter out = new BufferedWriter(outStream);
 
+
+            String msg = new String();
             while (!in.ready());
 
+            int i = 0;
             System.out.println();
             while (in.ready()) {
+                if (i == 0) {
+                    request = in.readLine();
+                    msg = getMsg(request);
+                    System.out.println(request);
+
+                }
                 System.out.println(in.readLine());
+                i++;
             }
 
             out.write("HTTP/1.1 200 OK\n");
             out.write("Content-Type: text/html; charset=utf-8\n");
+            out.write("Content length: " + msg.length() + "\n");
             out.write("\n");
-            out.write("<p>Агга</p>\n");
+            out.write(msg + "\n");
             out.flush();
 
             in.close();
             out.close();
             client.close();
         }
+    }
+
+    public static String getMsg(String request) throws IOException{
+        String[] array = request.split(" ");
+        BufferedReader in = new BufferedReader(new FileReader(array[1].substring(1)));
+        StringBuilder msg = new StringBuilder();
+        String str;
+        while ((str = in.readLine()) != null) {
+            msg.append(str);
+        }
+        in.close();
+        return msg.toString();
     }
 }
